8d3d7422d363e79fb568345fe3573289f6925678
ridhoadtyaa <r2603an@gmail.com>
2022-11-22 22:20:05 +0700

    init

---
+++ src/componets/IndonesianCities.jsx
@@ -0,0 +1,34 @@
+import {ScrollView} from 'react-native';
+import DestinationsCard from './card/DestinationCard';
+
+const IndonesianCiteis = () => {
+  return (
+    <ScrollView
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      style={{marginLeft: 20, paddingVertical: 20}}>
+      <DestinationsCard
+        name="Bali"
+        image={require('../assets/img/indonesia/bali.png')}
+      />
+      <DestinationsCard
+        name="Jakarta"
+        image={require('../assets/img/indonesia/jakarta.png')}
+      />
+      <DestinationsCard
+        name="Yogyakarta"
+        image={require('../assets/img/indonesia/jogja.png')}
+      />
+      <DestinationsCard
+        name="Bandung"
+        image={require('../assets/img/indonesia/bandung.png')}
+      />
+      <DestinationsCard
+        name="Medan"
+        image={require('../assets/img/indonesia/medan.png')}
+      />
+    </ScrollView>
+  );
+};
+
+export default IndonesianCiteis;
